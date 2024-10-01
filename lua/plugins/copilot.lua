b099d911b0417b41fde5460e0c21ece92d214abf
Giovanni Panasiti <giova.panasiti@gmail.com>
2024-10-02 01:37:23 +0200

    add copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,14 @@
 return {
     {
         "github/copilot.vim",
+        event = "InsertEnter",
+        autoStart = true,
+        run = function()
+            require("copilot").start()
+        end,
+        config = function()
+            vim.api.nvim_set_keymap("i", "<C-.>", "<cmd>lua require('copilot').complete()<CR>", { noremap = true, silent = true }) 
+        end,
+        
     },
 }
